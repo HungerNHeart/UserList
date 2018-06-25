ba8afb889a1302c2259666868898cc0f684305f2
Guru Karthi <gurukarthi.android@gmail.com>
2018-06-25 17:12:27 +0530

    #First commit

---
+++ app/src/main/java/com/droidmob/zohousers/LaunchActivity.kt
@@ -0,0 +1,12 @@
+package com.droidmob.zohousers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+}
